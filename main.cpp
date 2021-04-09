f68bf19b496b0c97271b9b6f06ba3ca5db813961
nombre15 <81451283+nombre15@users.noreply.github.com>
2021-04-09 14:01:59 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <math.h>
+#include <time.h>
+using namespace std;
+
+void dado1(void);
+void dado2(void);
+void suma (void);
+
+int sum = 0;
+int numero1 = 0;
+int numero2 = 0;
+
+
+int main(){
+
+   thread dado1_thread (dado1);
+   this_thread::sleep_for (chrono::seconds(1));
+   thread dado2_thread (dado2);
+   dado1_thread.join();
+   dado2_thread.join();
+
+}
+
+void dado1(){
+
+    srand(time(0));
+    for(int i = 0; i <= 10; i++){
+
+    i = 1 + rand() %6;
+    numero1 = i;
+    cout << "dado 1: " << i << endl;
+    this_thread::sleep_for (chrono::seconds(3));
+   }
+}
+
+void dado2(){
+
+    srand(time(0));
+    for(int x = 0; x >= 0; x--){
+
+    x = 1 + rand() %6;
+    numero2 = x;
+    cout << "dado 2: " << x << endl;
+    suma();
+    this_thread::sleep_for (chrono::seconds(3));
+    }
+}
+
+void suma(){
+
+   sum = numero1 + numero2;
+   cout << "total ==> " << sum << endl;
+}
+
+
+
+
